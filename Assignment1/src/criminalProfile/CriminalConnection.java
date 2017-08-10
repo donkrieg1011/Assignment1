28a04a382602e8e2e1ac0345cccecb1e1645740a
Ciaran.Madden <Ciaran.Madden@B106-01.soit.local>
2017-08-10 14:10:02 +1200

    Update CriminalConnection class

--- Assignment1/src/criminalProfile/CriminalConnection.java
+++ Assignment1/src/criminalProfile/CriminalConnection.java
@@ -1,4 +1,3 @@
-
 package criminalProfile;
 
 import java.sql.*;
@@ -11,12 +10,44 @@ public class CriminalConnection {
 	private StringBuilder list;
 	
 	public CriminalConnection() throws Exception {		
+	    String hostName = "localhost";
+		String user = "root";
+		String password = "";
+		String dbName = "ass1";
+		String dburl = "jdbc:mysql://" + hostName + ":3306/" + dbName + "?autoReconnect=true&useSSL=false";;
+		DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+		myConn = DriverManager.getConnection(dburl, user, password);
+		System.out.println("Successful connection to: " + dburl);
 	}
 	
-	public void addCriminal(CriminalInfo newCriminal) throws Exception {
+	public void addEntry(CriminalInfo newCriminal) throws Exception {
+		PreparedStatement myStmt = null;
+
+		myStmt = myConn.prepareStatement("insert into criminal_info"
+				+ " (first_name, last_name, nationality, crime, date_of_birth)"
+				+ " values (?, ?, ?, ?, ?)");
+		
+		try {
+			String dateFormat = "yyyy-MM-dd";
+			SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+			Date dateOfBirth = Date.valueOf(sdf.format(CriminalInfo.getDOB()));
+			
+			myStmt.setString(1, CriminalInfo.getFirstName());
+			myStmt.setString(2, CriminalInfo.getLastName());
+			myStmt.setString(3, CriminalInfo.getNationality());
+			myStmt.setString(4, CriminalInfo.getCrime());
+			myStmt.setDate(5, dateOfBirth);
+
+			myStmt.executeUpdate();			
+		}
+		finally {
+			myConn.close();
+		}
 	}
 	
 	public StringBuilder searchCriminals(String lastName) throws Exception {
+		return list;
 		
 	}
+	
 }
