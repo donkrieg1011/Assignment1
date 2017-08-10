c74b0675f6389526e67aabd31a564b6f584bb4ff
dinh.hoang2 <dinh.hoang2@B106-03.soit.local>
2017-08-10 14:09:19 +1200

    Added method content in CriminalConnection

--- Assignment1/src/criminalProfile/CriminalConnection.java
+++ Assignment1/src/criminalProfile/CriminalConnection.java
@@ -14,9 +14,41 @@ public class CriminalConnection {
 	}
 	
 	public void addCriminal(CriminalInfo newCriminal) throws Exception {
+		
 	}
 	
 	public StringBuilder searchCriminals(String lastName) throws Exception {
-		
+		list = new StringBuilder();
+		 
+		PreparedStatement myStmt = null;
+		ResultSet myRs = null;
+
+		try {
+			myStmt = myConn.prepareStatement("select * from criminal_info where last_name like ?  order by last_name");
+			
+			myStmt.setString(1, lastName);
+			
+			myRs = myStmt.executeQuery();
+			if(!myRs.next()){
+				list.append("NONE");
+			}
+			myRs.beforeFirst();
+			while (myRs.next()) {
+				String myFirstName = myRs.getString("first_name");
+	            String myLastName = myRs.getString("last_name");
+	            String myNationality = myRs.getString("nationality");
+	            String myCrime = myRs.getString("crime");
+	            Date myDOB = myRs.getDate("date_of_birth");
+	            
+	            CriminalInfo tempCriminal = new CriminalInfo(myLastName, myFirstName, myNationality, myCrime, myDOB);
+	    		
+	    		list.append(tempCriminal.toString());
+	    		
+			}
+			return list;
+		}
+		finally {
+			myConn.close();
+		}
 	}
 }
